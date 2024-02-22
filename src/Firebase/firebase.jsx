bbef06ab8fdae059c65c8048431a047ab5a68b77
Araddhya <1941012880.araddhya@gmail.com>
2024-02-22 11:22:41 +0530

    4th commit

--- src/Firebase/firebase.jsx
+++ src/Firebase/firebase.jsx
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app"
 import {getAuth} from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 const firebaseConfig = {
-  apiKey: "AIzaSyA6a2fcxyxDmOHI08P2l71_txWioZ9si5c",
-  authDomain: "fir-auth-111-cbbca.firebaseapp.com",
-  projectId: "fir-auth-111-cbbca",
-  storageBucket: "fir-auth-111-cbbca.appspot.com",
-  messagingSenderId: "389065020019",
-  appId: "1:389065020019:web:75301f14c2fc06ef395006",
-  measurementId: "G-TVGR211K3L"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 }
+console.log(process.env)
+console.log(firebaseConfig)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
